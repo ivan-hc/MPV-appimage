78d867694a546de74bf0bedabe299b413802c880
iVAN <88724353+ivan-hc@users.noreply.github.com>
2023-07-16 21:17:42 +0200

    Update mpv-junest.sh

--- mpv-junest.sh
+++ mpv-junest.sh
@@ -30,12 +30,12 @@ wget -q https://archlinux.org/mirrorlist/?country="$(echo $COUNTRY)" -O - | sed 
 # SET THE LOCALE (DON'T TOUCH THIS)
 #sed "s/# /#>/g" ./.junest/etc/locale.gen | sed "s/#//g" | sed "s/>/#/g" >> ./locale.gen # UNCOMMENT TO ENABLE ALL THE LANGUAGES
 #sed "s/#$(echo $LANG)/$(echo $LANG)/g" ./.junest/etc/locale.gen >> ./locale.gen # ENABLE ONLY YOUR LANGUAGE, COMMENT IF YOU NEED MORE THAN ONE
-#rm ./.junest/etc/locale.gen
+rm ./.junest/etc/locale.gen
 #mv ./locale.gen ./.junest/etc/locale.gen
 rm ./.junest/etc/locale.conf
-echo "LANG=$LANG" >> ./.junest/etc/locale.conf
+#echo "LANG=$LANG" >> ./.junest/etc/locale.conf
 sed -i 's/LANG=${LANG:-C}/LANG=$LANG/g' ./.junest/etc/profile.d/locale.sh
-./.local/share/junest/bin/junest -- sudo pacman --noconfirm -S glibc gzip
+#./.local/share/junest/bin/junest -- sudo pacman --noconfirm -S glibc gzip
 #./.local/share/junest/bin/junest -- sudo locale-gen
 
 # VERSION NAME, BY DEFAULT THIS POINTS TO THE NUMBER, CHANGE 'REPO' TO 'core', 'extra'...
