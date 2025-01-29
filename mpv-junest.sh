12aca383ac701dc42c53aa0133bf53a890b42f43
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-01-29 22:13:49 +0100

    Update mpv-junest.sh - add libSDL

--- mpv-junest.sh
+++ mpv-junest.sh
@@ -14,7 +14,7 @@ BINSAVED="SAVEBINSPLEASE"
 SHARESAVED="SAVESHAREPLEASE"
 lib_audio_keywords="alsa jack pipewire pulse"
 lib_browser_launcher="gio-launch-desktop libasound.so libatk-bridge libatspi libcloudproviders libdb- libdl.so libedit libepoxy libgtk-3.so.0 libjson-glib libnssutil libpthread.so librt.so libtinysparql libwayland-cursor libX11-xcb.so libxapp-gtk3-module.so libXcursor libXdamage libXi.so libxkbfile.so libXrandr p11 pk"
-LIBSAVED="libglslang-default-resource-limits libsoxr.so libsodium.so libFLAC.so $lib_audio_keywords $lib_browser_launcher"
+LIBSAVED="libglslang-default-resource-limits libsoxr.so libsodium.so libFLAC.so libSDL $lib_audio_keywords $lib_browser_launcher"
 
 [ -n "$lib_browser_launcher" ] && DEPENDENCES="$DEPENDENCES xapp hicolor-icon-theme"
 
